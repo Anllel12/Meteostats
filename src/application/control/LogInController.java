3b8a6eeca4c35988b49f992313ed8fa4f7cb0404
@alxgarci <alejandrogarci2000@gmail.com>
2022-11-14 12:02:10 +0100

    +Validacion visual de cada campo +Validadores personalizados

--- src/application/control/LogInController.java
+++ src/application/control/LogInController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
+import com.jfoenix.validation.base.ValidatorBase;
 
 import application.main.GestionGson;
 import application.main.Usuario;
@@ -38,6 +39,72 @@ public class LogInController {
 	@FXML
 	private Pane ventanaRegistro;
 	
+	crearValidacionUsuario cv;
+	crearValidacionUsuario cv2;
+	
+	@FXML // This method is called by the FXMLLoader when initialization is complete
+    void initialize() {
+		/**
+		 * VALIDADORES DE CAMPOS VACIOS, PREFIERO LOS OTROS CREADOS
+		 * 
+		 RequiredFieldValidator rfv = new RequiredFieldValidator();
+		rfv.setMessage("No puede estar vacío");
+        txtUser.getValidators().add(rfv);
+        txtUser.focusedProperty().addListener((ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) -> {
+            if (!newValue) {
+                txtUser.validate();
+            }
+        });
+        txtPass.getValidators().add(rfv);
+        txtPass.focusedProperty().addListener((ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) -> {
+            if (!newValue) {
+                txtPass.validate();
+            }
+        });
+		 */
+		
+        
+		cv = new crearValidacionUsuario();
+        txtUser.getValidators().add(cv);
+        cv.setMsg("Usuario no encontrado");
+        
+        cv2 = new crearValidacionUsuario();
+        txtPass.getValidators().add(cv2);
+        cv2.setMsg("Contraseña incorrecta");
+        
+	}
+	
+	private class crearValidacionUsuario extends ValidatorBase {
+		// Creando nuevo validador para mostrar error si el usuario no se encuentra
+			Boolean error = false;
+			String txt = "";
+			@Override
+			protected void eval() {
+				evalUser();
+			}
+			
+			public void setError(Boolean b) {
+				this.error = b;
+			}
+			
+			public void setMsg(String msg) {
+				this.txt = msg;
+			}
+			
+			private void evalUser() {
+
+				hasErrors.set(false);
+				try {
+					if (error) {
+						setMessage(txt);
+						throw new Exception("Password dont match");
+					}
+				} catch (Exception e) {
+					hasErrors.set(true);
+				}
+			}
+		};
+	
 	void errorAlertCreator(String header, String context) {
 		Alert alert = new Alert(Alert.AlertType.ERROR);
 		alert.setTitle("ERROR");
@@ -52,31 +119,41 @@ public class LogInController {
         	String usuario = txtUser.getText().trim();
         	String contra = txtPass.getText().trim();
         	
+        	cv2.setError(false);
+    		cv.setError(false);
+    		txtPass.validate();
+    		txtUser.validate();
+        	
         	if (usuario.isEmpty() || contra.isEmpty()) {
 				errorAlertCreator("Error de inicio de sesión", "Existen campos vacíos");
         	} else {
+        		
         		GestionGson gg = new GestionGson();
         		switch (gg.loginUsuario(usuario, contra)) {
 					case GestionGson.LOG_CONTRA_INCORRECTA:
-						errorAlertCreator("Error de inicio de sesión", "Contraseña incorrecta");
+						cv2.setError(true);
+						txtPass.validate();
 						break;
 					case GestionGson.LOG_USUARIO_NO_ENCONTRADO:
-						errorAlertCreator("Error de inicio de sesión", "El nombre de usuario no corresponde a ninguna cuenta");
+						cv.setError(true);
+						txtUser.validate();
 						break;
 					case GestionGson.LOG_OK:
 						Usuario us = gg.getUsuarioActual();
 						errorAlertCreator("LOGGING OK", us.getUsuario());
+						
+						//TODO cargar ventana principal
+						FXMLLoader loader = new FXMLLoader(getClass().getResource("ventanaPrincipal.fxml"));
+			            view = loader.load();
+			            Stage st = new Stage();
+			            st.setScene(new Scene(view));
+			            st.centerOnScreen();
+			            st.show();
 						break;
 				}
         		
         		
-        		//TODO cargar ventana principal
-				FXMLLoader loader = new FXMLLoader(getClass().getResource("ventanaPrincipal.fxml"));
-	            view = loader.load();
-	            Stage st = new Stage();
-	            st.setScene(new Scene(view));
-	            st.centerOnScreen();
-	            st.show();
+        		
 			}
             
 
--- src/application/control/SignUpController.java
+++ src/application/control/SignUpController.java
@@ -4,15 +4,19 @@ import java.io.IOException;
 
 import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXTextField;
+import com.jfoenix.validation.RequiredFieldValidator;
+import com.jfoenix.validation.base.ValidatorBase;
 
 import application.main.GestionGson;
 import application.main.Usuario;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.control.TextInputControl;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
@@ -42,8 +46,86 @@ public class SignUpController {
 	@FXML // This method is called by the FXMLLoader when initialization is complete
     void initialize() {
 		seleccionRol.getItems().addAll("Usuario", "Administrador", "Tecnico");
+		
+		// Para validar que los campos no estan vacios
+		RequiredFieldValidator rfv = new RequiredFieldValidator();
+		
+        usuario.getValidators().add(rfv);
+        email.getValidators().add(rfv);
+        nombre.getValidators().add(rfv);
+        contrasena.getValidators().add(rfv);
+        
+        validacionContrasenas();
+        
+        apellido.getValidators().add(rfv);
+        seleccionRol.getValidators().add(rfv);
+        rfv.setMessage("Este campo es obligatorio");
+        
+        setListenerValidaction(usuario);
+        setListenerValidaction(email);
+        setListenerValidaction(nombre);
+        setListenerValidaction(apellido);
+        seleccionRol.focusedProperty().addListener((ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) -> {
+            if (!newValue) {
+                seleccionRol.validate();
+            }
+        });
+		
     }
 	
+	private void validacionContrasenas() {
+		// Creamos un nuevo validador desde cero, para comparar pass1 y pass2 y mostrar
+		// el mensaje de error
+		ValidatorBase vb = new ValidatorBase() {
+			
+			@Override
+			protected void eval() {
+				if (srcControl.get() instanceof TextInputControl) {
+					evalPassword();
+				}
+			}
+			
+			private void evalPassword() {
+				TextInputControl textField = (TextInputControl) srcControl.get();
+				String pass2 = textField.getText();
+				String pass1 = contrasena.getText();
+				hasErrors.set(false);
+				try {
+					if (!pass1.isEmpty()) {
+						if (!pass1.equals(pass2)) {
+							setMessage("Contraseñas no coinciden");
+							throw new Exception("Password dont match");
+						}
+					}
+				} catch (Exception e) {
+					hasErrors.set(true);
+				}
+			}
+		};
+		
+		contrasena2.getValidators().add(vb);
+        contrasena.focusedProperty().addListener((ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) -> {
+            if (!newValue) {
+                contrasena2.validate();
+            }
+        });
+        contrasena2.focusedProperty().addListener((ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) -> {
+            if (!newValue) {
+                contrasena2.validate();
+            }
+        });
+		
+	}
+
+	private void setListenerValidaction(JFXTextField jf) {
+		jf.focusedProperty().addListener((ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) -> {
+            if (!newValue) {
+                jf.validate();
+            }
+        });
+		
+	}
+
 	void errorAlertCreator(String header, String context) {
 		Alert alert = new Alert(Alert.AlertType.ERROR);
 		alert.setTitle("ERROR");
@@ -81,15 +163,15 @@ public class SignUpController {
 		String e = email.getText().trim();
 		String n = nombre.getText().trim();
 		String c = contrasena.getText().trim();
-		String c2 = contrasena2.getText().trim();
 		String ap = apellido.getText().trim();
 		String selected = seleccionRol.getSelectionModel().getSelectedItem();
-		if (!c.equals(c2)) {
+		
+		if (!contrasena2.validate()) {
 			
 			errorAlertCreator("Error en el registro", "Las contraseñas introducidas no coinciden");
 			
-		} else if (u.isEmpty() || e.isEmpty() || n.isEmpty() || c.isEmpty() || c2.isEmpty()
-				|| ap.isEmpty() || selected.isEmpty()) {
+		} else if (!usuario.validate() || !email.validate() || !nombre.validate() || !contrasena.validate()
+				|| !apellido.validate() || !seleccionRol.validate()) {
 			
 			errorAlertCreator("Error en el registro", "Existen campos obligatorios vacíos");
 			
@@ -115,7 +197,6 @@ public class SignUpController {
 			} else if (isOk == GestionGson.REG_ERROR_MISMO_USUARIO) {
 				errorAlertCreator("Error en el registro", "El usuario introducido ya existe en la BBDD");
 				// Pone el texto del textfield de color rojo, puede ser util
-				usuario.setStyle("-fx-text-fill: red");
 			} else {
 				loadLogin(null);
 			}
