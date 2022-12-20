0f2e8e5578e4f401cdc38c0d803bc4bed8428080
Angel Esquinas <58303196+Anllel12@users.noreply.github.com>
2022-12-20 13:06:53 +0100

    CSS y responsive

--- src/application/control/AdministradorController.java
+++ src/application/control/AdministradorController.java
@@ -1,11 +1,18 @@
 package application.control;
 
+import java.io.IOException;
+
 import java.util.Vector;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextArea;
 
 import application.main.MensajeObj;
 import application.main.Mensajeria;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.TabPane;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
@@ -27,7 +34,12 @@ public class AdministradorController {
     
     @FXML
     private TableColumn<MensajeObj, String> status;
-
+    
+    @FXML
+    private Button btnEnviarM;
+    
+    @FXML
+    private JFXTextArea txtMensaje;
 
     void errorAlertCreator(String header, String context) {
 		Alert alert = new Alert(Alert.AlertType.ERROR);
@@ -36,6 +48,20 @@ public class AdministradorController {
 		alert.setContentText(context);
 		alert.showAndWait();
 	}
+    
+    @FXML
+    void enviarMensaje(ActionEvent event) throws IOException{
+    	Mensajeria mg = new Mensajeria();
+    	int from = 0;
+    	int to = 0;    	
+    	String mensaje = txtMensaje.getText().trim();
+    	int isOk = mg.writeNewMessage(mensaje, from, to);
+		if (isOk == Mensajeria.ERROR_ESCRITURA) {
+			errorAlertCreator("Error","No se ha podido enviar el mensaje");
+		} else if (isOk == Mensajeria.ESCRITURA_OK) {
+			errorAlertCreator("Completado","El mensaje se ha enviado correctamente");
+		}
+    }
     
     @FXML
 	void initialize() {
@@ -77,7 +103,7 @@ public class AdministradorController {
 		Mensajeria mensajeria = new Mensajeria();
 		Vector<MensajeObj> mensajes = mensajeria.getMessages(Mensajeria.USER_ADMIN);
 		
-		tbMsg.getItems().setAll(mensajes);	
+		tbMsg.getItems().setAll(mensajes);
 		
 	}
 	
